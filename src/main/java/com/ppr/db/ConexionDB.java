23890cc948842efda7c026440edf1a1670f2ed41
Fernando José Noceti <fer.j.noceti@gmail.com>
2023-11-08 04:30:55 -0300

    Update ConexionDB.java

--- src/main/java/com/ppr/db/ConexionDB.java
+++ src/main/java/com/ppr/db/ConexionDB.java
@@ -1,24 +1,55 @@
 package com.ppr.db;
 
-import java.sql.DriverManager;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+
 public class ConexionDB {
 
     Connection connection = null;
-    String url = "jdbc:mysql://localhost:3306/escuela";
-    String user = "root";
-    String password = "root";
 
+    String jdbcUrl = "";
+    String username = "";
+    String password = "";
 
-
-    public ResultSet runQuery(){
-
+    public ConexionDB() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
-    public ResultSet runQueryWithParams() {
+    public Connection getConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                connection = DriverManager.getConnection(jdbcUrl, username, password);
+            }
+            return connection;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
+    public void closeConnection() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void closeResultSet(ResultSet resultSet) {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
