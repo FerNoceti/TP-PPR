22205b9549092ad4756dc03a136302fdd6053ad9
Fernando José Noceti <fer.j.noceti@gmail.com>
2023-11-08 06:06:14 -0300

    Create Docente.java

---
+++ src/main/java/com/ppr/model/Docente.java
@@ -0,0 +1,42 @@
+package com.ppr.model;
+
+import java.sql.Timestamp;
+
+public class Docente extends Persona{
+    private int idDocente;
+    private String cv;
+
+    public Docente() {
+        super();
+    }
+
+    public Docente(int idPersona, int dni, String nombre, String apellido, Timestamp fechaNacimiento, int idDocente, String cv) {
+        super(idPersona, dni, nombre, apellido, fechaNacimiento);
+        this.idDocente = idDocente;
+        this.cv = cv;
+    }
+
+    public int getIdDocente() {
+        return idDocente;
+    }
+
+    public void setIdDocente(int idDocente) {
+        this.idDocente = idDocente;
+    }
+
+    public String getCv() {
+        return cv;
+    }
+
+    public void setCv(String cv) {
+        this.cv = cv;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Docente{" +
+                "idDocente=" + idDocente +
+                ", cv='" + cv + '\'' +
+                '}';
+    }
+}
