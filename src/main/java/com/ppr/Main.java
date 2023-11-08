114f417ac861c1d35896c7b0d95c016202403691
Fernando José Noceti <fer.j.noceti@gmail.com>
2023-11-08 04:33:13 -0300

    Update Main.java

--- src/main/java/com/ppr/Main.java
+++ src/main/java/com/ppr/Main.java
@@ -1,11 +1,20 @@
 package com.ppr;
 
 import com.ppr.implementations.AlumnoDaoImp;
+import com.ppr.model.Alumno;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         AlumnoDaoImp alumnoDaoImp = new AlumnoDaoImp();
+        alumnoDaoImp.addAlumno(1);
         alumnoDaoImp.addAlumno(2);
+
+        List<Alumno> alumnos = alumnoDaoImp.getAllAlumnos();
+        for (Alumno alumno : alumnos) {
+            System.out.println(alumno);
+        }
 
 
 
