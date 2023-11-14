ffb60cea31874e4ba4906803f7845b7d355c1dbe
Fernando José Noceti <fer.j.noceti@gmail.com>
2023-11-14 17:58:30 -0300

    curso presencial

--- src/main/java/ppr/data/dao/implementations/CursoPresencialDaoImp.java
+++ src/main/java/ppr/data/dao/implementations/CursoPresencialDaoImp.java
@@ -1,4 +1,38 @@
 package ppr.data.dao.implementations;
 
-public class CursoPresencialDaoImp {
+import ppr.data.dao.interfaces.CursoPresencialDao;
+import ppr.model.CursoPresencial;
+
+import java.util.List;
+
+public class CursoPresencialDaoImp extends CursoDaoImp implements CursoPresencialDao {
+    @Override
+    public List<CursoPresencial> getAllCursosPresenciales() {
+        return null;
+    }
+
+    @Override
+    public CursoPresencial getCursoPresencial(int id) {
+        return null;
+    }
+
+    @Override
+    public boolean addCursoPresencial(CursoPresencial cursoPresencial) {
+        return false;
+    }
+
+    @Override
+    public void updateCursoPresencial(CursoPresencial cursoPresencial) {
+
+    }
+
+    @Override
+    public boolean deleteCursoPresencial(int id) {
+        return false;
+    }
+
+    @Override
+    public boolean existeCursoPresencial(int idCurso) {
+        return false;
+    }
 }
--- src/main/java/ppr/data/dao/interfaces/CursoPresencialDao.java
+++ src/main/java/ppr/data/dao/interfaces/CursoPresencialDao.java
@@ -1,4 +1,19 @@
 package ppr.data.dao.interfaces;
 
+import ppr.model.CursoPresencial;
+
+import java.util.List;
+
 public interface CursoPresencialDao extends CursoDao {
+    List<CursoPresencial> getAllCursosPresenciales();
+
+    CursoPresencial getCursoPresencial(int id);
+
+    boolean addCursoPresencial(CursoPresencial cursoPresencial);
+
+    void updateCursoPresencial(CursoPresencial cursoPresencial);
+
+    boolean deleteCursoPresencial(int id);
+
+    boolean existeCursoPresencial(int idCurso);
 }
