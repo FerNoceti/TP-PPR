8e39c2d605f73f8e01a5be36cddc3bd5f55d0bcf
Fernando José Noceti <fer.j.noceti@gmail.com>
2023-11-28 17:50:08 -0300

    Update Main.java

--- src/main/java/ppr/main/Main.java
+++ src/main/java/ppr/main/Main.java
@@ -16,6 +16,7 @@ public class Main {
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.pack();
             frame.setVisible(true);
+            frame.setSize(500, 500);
         });
     }
 }
